23959f38de4666d10201c3eba9f1848ec9e8608f
Joshua Sotto <joshuasottoelyen@gmail.com>
2024-07-02 23:36:30 +0800

    Post Create

--- pages/create.vue
+++ pages/create.vue
@@ -1,9 +1,9 @@
 <template>
-      <div class="py-15 w-66">
+      <div class="py-10 w-66">
         <h1 class="text-grey-darken-3 mb-5">Create Post</h1>
         <v-tabs clas="mt-5">
-          <v-tab class="px-5 text-capitalize"  prepend-icon="mdi-text">Text</v-tab>
-          <v-tab class="px-5 text-capitalize" prepend-icon="mdi-image">Media</v-tab>
+          <v-tab :to="{name: 'create-index'}" class="px-5 text-capitalize"  prepend-icon="mdi-text">Text</v-tab>
+          <v-tab :to="{name: 'create-media'}" class="px-5 text-capitalize" prepend-icon="mdi-image">Media</v-tab>
           <v-tab class="px-5 text-capitalize" prepend-icon="mdi-link-variant">Link</v-tab>
           <v-tab class="px-5 text-capitalize" prepend-icon="mdi-poll">Vote</v-tab>
         </v-tabs>
@@ -19,7 +19,7 @@ useHead({
 
 definePageMeta({
   layout:'authenticated',
-  middleware: ['auth'],
+  middleware: 'auth',
 })
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -39,20 +39,20 @@ useHead({
         </Suspense>
       </div>
       <div class="pb-15" flat>
-        <v-card class=" mb-5 rounded-lg border d-flex align-center justify-center flex-column pa-5" flat>
+        <v-card width="325" class=" mb-5 rounded-lg border d-flex align-center justify-center flex-column pa-5" flat>
           <h4 class="font-weight-medium">Invite your fellow alumni</h4>
           <v-btn rounded="lg" prepend-icon="mdi-plus" variant="outlined" color="primary" class="text-capitalize mt-3">Invite Alumni</v-btn>
         </v-card>
         <suspense>
           <div>
-              <HomeConnectionRequestDrawer class="mb-5"></HomeConnectionRequestDrawer>
-              <HomeSuggestionDrawer></HomeSuggestionDrawer>
+              <HomeConnectionRequestDrawer width="325" class="mb-5"></HomeConnectionRequestDrawer>
+              <HomeSuggestionDrawer width="325"></HomeSuggestionDrawer>
           </div>
           <template #fallback>
-            <div class="rounded-lg border mb-4 w-100" v-for="n in 5" style="overflow:hidden">
+            <v-card width="325" flat class="rounded-lg border mb-4" v-for="n in 5" style="overflow:hidden">
                 <v-skeleton-loader class="w-100" type="avatar, chip"></v-skeleton-loader>
                 <v-skeleton-loader type="actions"></v-skeleton-loader>
-            </div>
+            </v-card>
           </template>
         </suspense>
       </div>       
